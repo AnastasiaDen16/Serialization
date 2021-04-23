67bb700289d28f5fc2ac2f2bd0a6e0f031cfa584
Anastasia <anastdenisova16@gmail.com>
2021-04-23 16:37:13 +0300

    add project with serialization in json

---
+++ Serialization_Deserialization/Person.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+
+namespace Serialization_Deserialization
+{
+    [DataContract]
+    public class Person
+    {
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Surname { get; set; }
+        [DataMember]
+        public string Adress { get; set; }
+    }
+}
---
+++ Serialization_Deserialization/Program.cs
@@ -0,0 +1,39 @@
+﻿using Nancy.Json;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Json;
+
+namespace Serialization_Deserialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Serialization_DCJS();
+            Serialization_JSS();
+        }
+        public static void Serialization_DCJS()
+        {
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(Person));
+            MemoryStream stream = new MemoryStream();
+            serializer.WriteObject(stream, InitPerson());
+        }
+        public static void Serialization_JSS()
+        {
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            var json = serializer.Serialize(InitPerson());
+            Console.WriteLine(json);
+        }
+
+        public static Person InitPerson()
+        {
+            Person person = new Person
+            {
+                Name = "Denisova",
+                Surname = "Anastasia",
+                Adress = "Gomel"
+            };
+            return person;
+        }
+    }
+}
